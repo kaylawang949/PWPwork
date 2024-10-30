26ab96011ab3464675be6250e7c84b2ceecac7e5
Kayla W <149686676+kaylawang949@users.noreply.github.com>
2024-10-30 01:30:27 -0400

    Update chat1.py

--- TerminalChatCode/chat1.py
+++ TerminalChatCode/chat1.py
@@ -1,22 +1,25 @@
+#to interact with operation system
 import os
 
-# Use environment variables for pipe paths
+#paths for the named pipes
 pipe1 = os.getenv("PIPE1", "/tmp/pipe1")
 pipe2 = os.getenv("PIPE2", "/tmp/pipe2")
 
+#if READ_FIRST is set to 1, then it reads first
 read_first = os.getenv("READ_FIRST", "0")
 
-# Create named pipes if they do not exist
+#creating named pipes if they don't exist
 try:
     os.mkfifo(pipe1, 0o600)
-except FileExistsError:
-    print("Named pipe for pipe1 already exists!")
+except:
+    pass
 
 try:
     os.mkfifo(pipe2, 0o600)
-except FileExistsError:
-    print("Named pipe for pipe2 already exists!")
+except:
+    pass
 
+#based off of if READ_ONLY is set to 1 or not
 if read_first == "1":
     fifo1 = os.open(pipe1, os.O_RDONLY)
     fifo2 = os.open(pipe2, os.O_WRONLY)
@@ -24,21 +27,22 @@ else:
     fifo1 = os.open(pipe1, os.O_WRONLY)
     fifo2 = os.open(pipe2, os.O_RDONLY)
 
+
 while True:
     if read_first == "1":
-        # Read message from pipe first
+        #read and decode message first
         msg = os.read(fifo1, 1024)
-        msg_decoded = msg.decode().strip()
+        msg_decoded = msg.decode()
         
-        # Check if the exit message is received
+        #if the message received is exit then exit
         if msg_decoded.lower() == "exit":
             print("Other user exited. Exiting chat...")
             break
         
-        # Output the message to screen
+        #show message
         print(msg_decoded)
         
-        # Get message from input
+        #enter message
         msg = input(">")
         if msg.strip().lower() == "exit":
             print("Exiting chat...")
@@ -46,29 +50,25 @@ while True:
             break
         os.write(fifo2, str.encode(msg))
     else:
-        # Read message from input first
+        #enter message and exit if told to
         msg = input(">")
         if msg.strip().lower() == "exit":
             print("Exiting chat...")
             os.write(fifo1, str.encode("exit"))
             break
         
-        # Send message to pipe
+        #send message to pipe
         os.write(fifo1, str.encode(msg))
         
-        # Receive message from pipe
+        #receive message from pipe
         msg = os.read(fifo2, 1024)
-        msg_decoded = msg.decode().strip()
+        msg_decoded = msg.decode()
         
-        # Check if the exit message is received
+        #if told to exit then exit
         if msg_decoded.lower() == "exit":
             print("Other user exited. Exiting chat...")
             break
         
-        # Output the message to screen
+        #show message
         print(msg_decoded)
-
-# Clean up by closing the pipes
-os.close(fifo1)
-os.close(fifo2)
 
